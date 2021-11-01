372b5fb0f28cfce63a8c68d1a57b7c7be2b701df
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2021-11-01 15:57:10 -0700

    fix logging when running module

--- brultech_serial2mqtt/__main__.py
+++ brultech_serial2mqtt/__main__.py
@@ -3,10 +3,15 @@ import logging
 
 from brultech_serial2mqtt import BrultechSerial2MQTT
 
-logger = logging.getLogger(__name__)
-logger.addHandler(logging.StreamHandler())
+logger = logging.getLogger("brultech_serial2mqtt")
 
 if __name__ == "__main__":
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(
+        logging.Formatter("'%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    )
+    logger.addHandler(stream_handler)
+
     bs2m = BrultechSerial2MQTT()
     asyncio.get_event_loop().run_until_complete(bs2m.start())
     asyncio.get_event_loop().run_forever()
