950c86076186c4312ffce5173d989db2c044ed82
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2023-12-24 09:46:59 -0800

    refactor management of home assistant lifecycle with mqtt

--- brultech_serial2mqtt/mqtt.py
+++ brultech_serial2mqtt/mqtt.py
@@ -2,12 +2,38 @@ import asyncio
 import logging
 import pprint
 
+from asyncio.tasks import Task
+from typing import Awaitable, Callable, Optional
+
 from aiomqtt import Client as MQTTClient
 from aiomqtt.error import MqttError
 from brultech_serial2mqtt.config import MQTTConfig
 from brultech_serial2mqtt.device import DeviceManager
 
 logger = logging.getLogger(__name__)
+
+
+async def manage_home_assistant_lifecycle(
+    config: MQTTConfig, mqtt_client: MQTTClient, device_manager: DeviceManager
+) -> Optional[Task[None]]:
+    if not config.home_assistant.enable:
+        logger.info(
+            "Home Assistant dicovery configuration is disabled.  Not publishing configuration."
+        )
+        return None
+
+    async def _on_birth() -> None:
+        logger.debug(
+            "Home Assistant has re-connected to the mqtt server.  Resending discovery configuration..."
+        )
+        await asyncio.create_task(
+            publish_home_assistant_discovery_config(config, mqtt_client, device_manager)
+        )
+
+    await publish_home_assistant_discovery_config(config, mqtt_client, device_manager)
+    return asyncio.create_task(
+        subscribe_to_home_assistant_birth(config, mqtt_client, _on_birth)
+    )
 
 
 async def publish_birth_message(
@@ -52,5 +78,34 @@ async def publish_home_assistant_discovery_config(
     except Exception as exc:
         logger.exception(
             "Exception caught while attempting to publish Home Assistant discovery configuration!",
+            exc_info=exc,
+        )
+
+
+async def subscribe_to_home_assistant_birth(
+    config: MQTTConfig, mqtt_client: MQTTClient, on_birth: Callable[[], Awaitable[None]]
+) -> None:
+    try:
+        async with mqtt_client.messages() as messages:
+            await mqtt_client.subscribe(
+                config.home_assistant.birth_message.topic,
+                qos=config.home_assistant.birth_message.qos,
+            )
+            async for message in messages:
+                if (
+                    message.topic.matches(config.home_assistant.birth_message.topic)
+                    and message.payload == config.home_assistant.birth_message.payload
+                ):
+                    await on_birth()
+    except asyncio.CancelledError:
+        pass
+    except MqttError as exc:
+        logger.debug(
+            "MqttError while listenting/publishing for Home Assistant birth!",
+            exc_info=exc,
+        )
+    except Exception as exc:
+        logger.exception(
+            "Exception caught while listenting/publishing for Home Assistant birth!",
             exc_info=exc,
         )
